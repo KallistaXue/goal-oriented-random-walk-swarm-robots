1d9e7adde15ce56ebddb66976a3d345392e4bb36
Kallista Xue <ax2170@columbia.edu>
2023-08-25 09:29:19 -0400

    Create sensor_test.cpp

---
+++ controllers/sensor_test/sensor_test.cpp
@@ -0,0 +1,127 @@
+// File:          sensor_test.cpp
+// Date:
+// Description:
+// Author:
+// Modifications:
+
+// You may need to add webots include files such as
+// <webots/DistanceSensor.hpp>, <webots/Motor.hpp>, etc.
+// and/or to add some other includes
+//#include <webots/Robot.hpp>
+
+// include headers
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <webots/device.h>
+#include <webots/distance_sensor.h>
+#include <webots/led.h>
+#include <webots/motor.h>
+#include <webots/nodes.h>
+#include <webots/robot.h>
+
+
+// All the webots classes are defined in the "webots" namespace
+using namespace webots;
+
+// distance sensor defination
+#define DISTANCE_SENSORS_NUMBER 8
+static WbDeviceTag distance_sensors[DISTANCE_SENSORS_NUMBER];
+static double distance_sensors_values[DISTANCE_SENSORS_NUMBER];
+static const char *distance_sensors_names[DISTANCE_SENSORS_NUMBER] = {"ps0", "ps1", "ps2", "ps3", "ps4", "ps5", "ps6", "ps7"};
+
+// motor
+static WbDeviceTag left_motor, right_motor;
+
+#define LEFT 0
+#define RIGHT 1
+#define MAX_SPEED 6.28
+static double speeds[2];
+
+
+static int get_time_step() {
+  static int time_step = -1;
+  if (time_step == -1)
+    time_step = (int)wb_robot_get_basic_time_step();
+  return time_step;
+}
+
+static void step() {
+  if (wb_robot_step(get_time_step()) == -1) {
+    wb_robot_cleanup();
+    exit(EXIT_SUCCESS);
+  }
+}
+
+static void init_devices() {
+  int i;
+  for (i = 0; i < DISTANCE_SENSORS_NUMBER; i++) {
+    distance_sensors[i] = wb_robot_get_device(distance_sensors_names[i]);
+    wb_distance_sensor_enable(distance_sensors[i], get_time_step());
+  }
+
+
+  // get a handler to the motors and set target position to infinity (speed control).
+  left_motor = wb_robot_get_device("left wheel motor");
+  right_motor = wb_robot_get_device("right wheel motor");
+  wb_motor_set_position(left_motor, INFINITY);
+  wb_motor_set_position(right_motor, INFINITY);
+  wb_motor_set_velocity(left_motor, 0.0);
+  wb_motor_set_velocity(right_motor, 0.0);
+
+  step();
+}
+
+static void get_sensor_input() {
+  int i;
+  for (i = 0; i < DISTANCE_SENSORS_NUMBER; i++) {
+    distance_sensors_values[i] = wb_distance_sensor_get_value(distance_sensors[i]);
+  }
+}
+
+// This is the main program of your controller.
+// It creates an instance of your Robot instance, launches its
+// function(s) and destroys it at the end of the execution.
+// Note that only one instance of Robot should be created in
+// a controller program.
+// The arguments of the main function can be specified by the
+// "controllerArgs" field of the Robot node
+int main(int argc, char **argv) {
+  // create the Robot instance.
+  Robot *robot = new Robot();
+
+  // get the time step of the current world.
+  int timeStep = (int)robot->getBasicTimeStep();
+
+  // You should insert a getDevice-like function in order to get the
+  // instance of a device of the robot. Something like:
+  //  Motor *motor = robot->getMotor("motorname");
+  //  DistanceSensor *ds = robot->getDistanceSensor("dsname");
+  //  ds->enable(timeStep);
+
+  wb_robot_init();
+
+  printf("Default controller of the e-puck robot started...\n");
+
+  init_devices();
+
+
+  // Main loop:
+  // - perform simulation steps until Webots is stopping the controller
+  while (robot->step(timeStep) != -1) {
+    // Read the sensors:
+    // Enter here functions to read sensor data, like:
+    //  double val = ds->getValue();
+
+    // Process sensor data here.
+printf("1\n");
+    // Enter here functions to send actuator commands, like:
+    //  motor->setPosition(10.0);
+  };
+
+  // Enter here exit cleanup code.
+
+  delete robot;
+  return 0;
+}
